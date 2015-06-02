aedc0b3a5279a1f34e186ddec663421fe8cc6a23
korenevskiy <koreshs@mail.ru>
2015-06-02 17:11:12 +0400

    Joomla!, 3.4.1

---
+++ logs/joomla_update.php
@@ -0,0 +1,42 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-08-03 21:17:41 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-08-03T21:17:41+00:00	INFO	update	Обновление запущено пользователем Super User (964). Предыдущая версия 3.2.3.
+2014-08-03T21:17:41+00:00	INFO	update	Загрузка файла обновления с .
+2014-08-03T21:17:57+00:00	INFO	update	Обновление запущено пользователем Super User (964). Предыдущая версия 3.2.3.
+2014-08-03T21:17:57+00:00	INFO	update	Файл Joomla_3.3.3-Stable-Update_Package.zip успешно загружен.
+2014-08-03T21:17:57+00:00	INFO	update	Начало установки новой версии.
+2014-08-03T21:17:58+00:00	INFO	update	Обновление запущено пользователем Super User (964). Предыдущая версия 3.2.3.
+2014-08-03T21:17:58+00:00	INFO	update	Файл Joomla_3.3.3-Stable-Update_Package.zip успешно загружен.
+2014-08-03T21:17:58+00:00	INFO	update	Файл Joomla_3.3.3-Stable-Update_Package.zip успешно загружен.
+2014-08-03T21:17:58+00:00	INFO	update	Начало установки новой версии.
+2014-08-03T21:18:18+00:00	INFO	update	Завершение установки.
+2014-08-03T21:18:19+00:00	INFO	update	Выполнение запроса из файла 3.3.0-2014-02-16. Текст запроса: ALTER TABLE `#__users` ADD COLUMN `requireReset` tinyint(4) NOT NULL DEFAULT 0 C.
+2014-08-03T21:18:19+00:00	INFO	update	Выполнение запроса из файла 3.3.0-2014-04-02. Текст запроса: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2014-08-03T21:18:19+00:00	INFO	update	Удаление устаревших файлов и директорий.
+2014-08-03T21:18:23+00:00	INFO	update	Cleaning up after installation.
+2014-08-03T21:18:23+00:00	INFO	update	Обновление до версии 3.3.3 завершено.
+2015-06-02T12:18:06+00:00	INFO	update	Обновление запущено пользователем Super User (692). Предыдущая версия 3.3.3.
+2015-06-02T12:18:06+00:00	INFO	update	Загрузка файла обновления с .
+2015-06-02T12:18:13+00:00	INFO	update	Файл Joomla_3.4.1-Stable-Update_Package.zip успешно загружен.
+2015-06-02T12:18:13+00:00	INFO	update	Начало установки новой версии.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Завершение установки.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Выполнение запроса из файла 3.3.4-2014-08-03. Текст запроса: ALTER TABLE `#__user_profiles` CHANGE `profile_value` `profile_value` TEXT NOT N.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Выполнение запроса из файла 3.3.6-2014-09-30. Текст запроса: INSERT INTO `#__update_sites` (`name`, `type`, `location`, `enabled`) VALUES ('J.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Выполнение запроса из файла 3.3.6-2014-09-30. Текст запроса: INSERT INTO `#__update_sites_extensions` (`update_site_id`, `extension_id`) VALU.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Выполнение запроса из файла 3.4.0-2014-08-24. Текст запроса: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Выполнение запроса из файла 3.4.0-2014-09-01. Текст запроса: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Выполнение запроса из файла 3.4.0-2014-09-01. Текст запроса: INSERT INTO `#__update_sites` (`name`, `type`, `location`, `enabled`) VALUES ('W.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Выполнение запроса из файла 3.4.0-2014-09-01. Текст запроса: INSERT INTO `#__update_sites_extensions` (`update_site_id`, `extension_id`) VALU.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Выполнение запроса из файла 3.4.0-2014-09-16. Текст запроса: ALTER TABLE `#__redirect_links` ADD header smallint(3) NOT NULL DEFAULT 301;.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Выполнение запроса из файла 3.4.0-2014-09-16. Текст запроса: ALTER TABLE `#__redirect_links` MODIFY new_url varchar(255);.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Выполнение запроса из файла 3.4.0-2014-10-20. Текст запроса: DELETE FROM `#__extensions` WHERE `extension_id` = 100;.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Выполнение запроса из файла 3.4.0-2014-12-03. Текст запроса: UPDATE `#__extensions` SET `protected` = '0' WHERE `name` = 'plg_editors-xtd_art.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Выполнение запроса из файла 3.4.0-2015-01-21. Текст запроса: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Выполнение запроса из файла 3.4.0-2015-02-26. Текст запроса: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2015-06-02T12:18:54+00:00	INFO 127.0.0.1	update	Удаление устаревших файлов и директорий.
+2015-06-02T12:18:56+00:00	INFO 127.0.0.1	update	Cleaning up after installation.
+2015-06-02T12:18:56+00:00	INFO 127.0.0.1	update	Обновление до версии 3.4.1 завершено.
